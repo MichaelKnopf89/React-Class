47bf716a0932a2ff9c02c046fbfc92e20e0511e6
mKnopfPortfolio <mknopfportfolio@gmail.com>
2022-05-19 23:08:43 -0400

    working with state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import CreateTweet from "./Components/CreateTweet";
+import TweetList from "./Components/TweetList";
 
 function App() {
+
+  const [name,setName] = useState("Mike");
+  const message = 'hello';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CreateTweet />
+      <TweetList  name={name} message = {message} />
     </div>
   );
 }
---
+++ src/Components/CreateTweet.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+
+const CreateTweet = ()=>{
+
+    const [textInput, setTextInput] = useState("");
+
+    const userInputHandler = (e)=>{
+        setTextInput(e.target.value);
+    }
+
+    return(
+        <form>
+            <textarea value={textInput} onChange={userInputHandler} cols="50" rows="5"></textarea>
+            <button>Submit</button>
+        </form>
+    )
+}
+
+export default CreateTweet;
