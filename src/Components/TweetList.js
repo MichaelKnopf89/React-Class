64cc28104e43d6fd52299bdc9c6cadb7d7cf0412
mKnopfPortfolio <mknopfportfolio@gmail.com>
2022-05-20 08:41:34 -0400

    add unique key with UUID

--- src/Components/TweetList.js
+++ src/Components/TweetList.js
@@ -5,7 +5,7 @@ const TweetList = ({name, tweets, setTweets})=>{
     return(
         <div className="tweetList">
             {tweets.map((tweet)=> (
-            <Tweet name = {name} tweet = {tweet} setTweets={setTweets} tweets = {tweets}
+            <Tweet name = {name} tweet = {tweet} setTweets={setTweets} tweets = {tweets} key={tweet.id}
              />))}
         </div>
     )
