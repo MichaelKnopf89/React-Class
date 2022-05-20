97d08ffa03ecdde71ca207d41fb99bd63f96aa11
mKnopfPortfolio <mknopfportfolio@gmail.com>
2022-05-20 08:59:01 -0400

    add sass

--- src/Components/Tweet.js
+++ src/Components/Tweet.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/tweet.scss"
 
 const Tweet = ({name, tweet, tweets, setTweets})=>{
 
@@ -8,7 +9,7 @@ const Tweet = ({name, tweet, tweets, setTweets})=>{
 
     return(
         <div className="tweet">
-            <h2>{name}</h2>
+            <h2 className="tweet-title">{name}</h2>
             <h3>{tweet.message}</h3>
             <button onClick={deleteTweet}>Delete</button>
             <button>Like</button>
